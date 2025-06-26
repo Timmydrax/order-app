a9d1f3e1b697c6eb8bd1d2549cbb4e0fa98204b6
Timmydrax <timmydrax@gmail.com>
2025-06-26 12:57:01 +0100

    CHORE: Skeletal Structures

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { menuArray } from "./data.js";
+
 const footerEl = document.getElementById("footer");
 const year = new Date().getFullYear();
 const pEl = document.createElement("p");
@@ -5,3 +7,9 @@ const pEl = document.createElement("p");
 pEl.classList.add("footer-content");
 pEl.textContent = `© ${year} Timmy_Drax. All rights reserved.`;
 footerEl.appendChild(pEl);
+
+console.log(menuArray);
+
+function getMenuArray() {
+    
+}
